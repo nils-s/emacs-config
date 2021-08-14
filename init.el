6ebceb9c4f7ad6c18ee8537fb44fe0b471876297
nils-s <28589333+nils-s@users.noreply.github.com>
2021-08-14 17:06:31 +0200

    switch default Common Lisp implementation to abcl

--- init.el
+++ init.el
@@ -67,11 +67,12 @@
 ;;; add Slime; using the version from Git (instead of MELPA), so we need to tell Emacs where to find it
 (add-to-list 'load-path "~/.slime") ; assumes Slime has been "git clone"-d into ~/.slime
 (require 'slime-autoloads)
-(setq inferior-lisp-program "sbcl")
+(setq inferior-lisp-program "abcl")
 ;;; use custom SBCL core including pre-loaded packages (sb-bsd-sockets, sb-posix, sb-introspect, sb-cltl2, asdf)
 ;;; see https://common-lisp.net/project/slime/doc/html/Loading-Swank-faster.html#Loading-Swank-faster
 (setq slime-lisp-implementations
-      '((sbcl ("sbcl" "--core" "sbcl.core-for-slime"))))
+      '((abcl ("abcl"))
+	(sbcl ("sbcl" "--core" "sbcl.core-for-slime"))))
 
 ;; -----------------------------------------------------------------
 
